4d2fea666b32b92f40249881e2bc2a7ac187f66f
Thomas Stoeckert <Thomas@Stoeckert.us>
2015-12-11 11:15:14 -0500

    Fixed to support older browsers

--- js/vhsface.js
+++ js/vhsface.js
@@ -2,10 +2,10 @@ function setDisplay(display){
 	var custom1 = document.getElementById("custom1");
 	var custom2 = document.getElementById("custom2");
 	if(display === 1){
-		custom1.hidden = false;
-		custom2.hidden = true;
+		custom1.style.display = "block";
+		custom2.style.display = "none";
 	} else {
-		custom1.hidden = true;
-		custom2.hidden = false;
+		custom1.style.display = "none";
+		custom2.style.display = "block";
 	}
 }
